969aa4e32434187c8f734ba0537daf102e2952fe
chori-kabilov <chorikabilov2@gmail.com>
2026-01-12 16:05:16 +0500

    Update Readme.md

--- Infrastructure/Migrations/20260108055507_AddSoftDeleteForTransactions.cs
+++ Infrastructure/Migrations/20260108055507_AddSoftDeleteForTransactions.cs
@@ -10,13 +10,28 @@ namespace Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "transactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
 
-        }
-
-        /// <inheritdoc />
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "transactions",
+                type: "timestamp with time zone",
+                nullable: true);
+        }/// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "transactions");
 
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "transactions");
         }
     }
 }
